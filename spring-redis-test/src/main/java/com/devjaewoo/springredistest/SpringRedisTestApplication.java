ccb66892bd7c7701330ff647ea948b2575b3d7ff
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-09-04 21:22:34 +0900

    Init spring-data-redis

---
+++ spring-redis-test/src/main/java/com/devjaewoo/springredistest/SpringRedisTestApplication.java
@@ -0,0 +1,13 @@
+package com.devjaewoo.springredistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisTestApplication.class, args);
+    }
+
+}
---
+++ spring-redis-test/src/test/java/com/devjaewoo/springredistest/SpringRedisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjaewoo.springredistest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
