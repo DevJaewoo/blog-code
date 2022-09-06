4ff002889f30d89e66f561e487c6ca1784a8cd03
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-09-06 15:55:25 +0900

    [spring-rabbitmq-test] Initial Commit

---
+++ spring-rabbitmq-test/src/main/java/com/devjaewoo/springrabbitmqtest/SpringRabbitmqTestApplication.java
@@ -0,0 +1,13 @@
+package com.devjaewoo.springrabbitmqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitmqTestApplication.class, args);
+    }
+
+}
---
+++ spring-rabbitmq-test/src/test/java/com/devjaewoo/springrabbitmqtest/SpringRabbitmqTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjaewoo.springrabbitmqtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
